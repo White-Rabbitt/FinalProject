9776a902dc7a8acf84f936c9aba4884247901e75
White_Rabbit <lacrimozzy989wr>
2021-08-15 19:52:43 +0300

    add files

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help="Выбор браузера: chrome или firefox")
+    parser.addoption('--language', action='store', default='en', help="выбор языка")
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    language = request.config.getoption('language')
+
+    if browser_name == "chrome":
+        print(f'\nЗапуск {browser_name} для теста...')
+        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError('--browser_name должно быть "chrome"')
+    yield browser
+    print('\nЗакрытие браузера...')
+    time.sleep(3)
+    browser.quit()
